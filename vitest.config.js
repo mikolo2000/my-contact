d23a7a8b93fcfebb1337347ca0fcfbfff807ad39
mikolo <mikolo.corn@gmail.com>
2025-02-22 11:22:02 +0100

    fixed a report bug

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,12 @@
 import {defineConfig} from "vitest/config";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
-    environment: 'jsdom'
-  }
+    environment: "jsdom",
+    coverage: {
+      provider: "istanbul",  
+      reporter: ["text", "json", "html"], 
+    },
+  },
 });
